6490edd80998f70523e708d6b73f691d2d00648c
ian-ramos <ramos.ianc@gmail.com>
2017-09-26 17:27:53 -0400

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,19 +8,26 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(session_params)
-    if @user.authenticate(session_params[:password])
+    # @user = User.create(user_params)
+    # if @user.authenticate(user_params[:password])
+    #   session[:user_id] = @user.id
+    #   redirect_to new_user_path
+    # else
+    #   redirect_to users_path
+    # end
+    if user_params[:password] == user_params[:password_confirmation]
+      @user = User.create(user_params)
       session[:user_id] = @user.id
-      redirect_to new_user_path
-    else
       redirect_to users_path
+    else
+      redirect_to new_user_path
     end
   end
 
   private
 
-  def session_params
-    params.require(:user).permit(:name, :password)
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
   end
 
 end
